7fc37bca66ae16f8d3c00969e95a087838533cd2
Philippe Timothe <PhilippeTimothe@gmail.com>
2025-03-06 12:34:10 -0500

    image debug

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://philiptimothe.github.io",
+  site: "https://philiptimothe.github.io/Philip-Timothe-Analyst-Page",
   base: "/Philip-Timothe-Analyst-Page/",
   build: {
     assets: "assets",
